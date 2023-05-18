e7cbc7c0bdca3a300c752f8af2ed6716b051f729
Jonathan Gil <jhongilp@gmail.com>
2023-05-17 19:58:22 -0500

    solution for part 2 of day 2

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,10 @@
-use std::collections::HashMap;
+// use aoc_1::part1::get_part1_answer;
+use aoc_1::part2::get_part2_answer;
 use std::fs;
-
-// AX -> rock-rock -> draw
-// AY -> rock-paper -> win
-// AZ -> rock-scissors -> loss
-// BX -> paper-rock -> loss
-// BY -> paper-paper -> draw
-// BZ -> paper-scissors -> win
-// CX -> scissors-rock -> win
-// CY -> scissors-paper -> loss
-// CZ -> scissors-scissors -> draw
 
 fn main() {
     println!("Hello, world!");
     let input = fs::read_to_string("input.txt").expect("Filed should be read");
-    // println!("Input data \n{}", input);
-    let game_scores = HashMap::from([
-        ("AX", 3),
-        ("AY", 6),
-        ("AZ", 0),
-        ("BX", 0),
-        ("BY", 3),
-        ("BZ", 6),
-        ("CX", 6),
-        ("CY", 0),
-        ("CZ", 3),
-    ]);
-
-    let total_points: i32 = input.lines().map(|x| {
-        let round: String = x.split_whitespace().collect();
-        let shape_score = match round.chars().nth(1) {
-            Some('X') => 1,
-            Some('Y') => 2,
-            Some('Z') => 3,
-            None => 0,
-            _ => 0, 
-        };
-        let score = match game_scores.get(round.as_str()) {
-            Some(x) => x.clone() + shape_score,
-            None => 0,
-        };
-        
-        score
-    }).sum();
-    println!("total points {:?}", total_points);
+    // get_part1_answer(input);
+    get_part2_answer(input);
 }
