a56a9cdd5fee0aa9e93df9d2c204ede75ac61ef1
Jonathan Gil <jhongilp@gmail.com>
2023-05-18 19:14:55 -0500

    solution for part 1 of day 3

--- src/part1.rs
+++ src/part1.rs
@@ -1,37 +1,24 @@
-use std::collections::HashMap;
-
 pub fn get_part1_answer(input: String) -> i32 {
-    let game_scores = HashMap::from([
-        ("AX", 3),
-        ("AY", 6),
-        ("AZ", 0),
-        ("BX", 0),
-        ("BY", 3),
-        ("BZ", 6),
-        ("CX", 6),
-        ("CY", 0),
-        ("CZ", 3),
-    ]);
+    let mut sum_of_priorities = 0;
+    input.lines().for_each(|rucksack| {
+        let compartment1 = rucksack[0..rucksack.len() / 2]
+            .chars()
+            .collect::<Vec<char>>();
+        let compartment2 = rucksack[rucksack.len() / 2..rucksack.len()]
+            .chars()
+            .collect::<Vec<char>>();
+        let item_in_both = compartment1
+            .iter()
+            .filter(|&item| compartment2.contains(item))
+            .collect::<Vec<&char>>();
 
-    let total_points: i32 = input
-        .lines()
-        .map(|x| {
-            let round: String = x.split_whitespace().collect();
-            let shape_score = match round.chars().nth(1) {
-                Some('X') => 1,
-                Some('Y') => 2,
-                Some('Z') => 3,
-                None => 0,
-                _ => 0,
-            };
-            let score = match game_scores.get(round.as_str()) {
-                Some(x) => x.clone() + shape_score,
-                None => 0,
-            };
-
-            score
-        })
-        .sum();
-    println!("total points part1: {:?}", total_points);
-    return total_points;
+        let offset = if item_in_both[0].is_ascii_uppercase() {
+            64 - 26 // 64 is the ascii code for A, and we want to start at 27
+        } else {
+            96
+        };
+        sum_of_priorities += item_in_both[0].clone() as u32 - offset;
+    });
+    println!("sum_of_priorities: {}", sum_of_priorities);
+    sum_of_priorities as i32
 }
