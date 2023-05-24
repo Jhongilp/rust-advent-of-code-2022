5100d9a1741723a8afa28e9714cf110908200412
Jonathan Gil <jhongilp@gmail.com>
2023-05-23 19:58:37 -0500

    split pairs to know the start and end of each one

--- src/part1.rs
+++ src/part1.rs
@@ -1,24 +1,12 @@
 pub fn get_part1_answer(input: String) -> i32 {
-    let mut sum_of_priorities = 0;
-    input.lines().for_each(|rucksack| {
-        let compartment1 = rucksack[0..rucksack.len() / 2]
-            .chars()
-            .collect::<Vec<char>>();
-        let compartment2 = rucksack[rucksack.len() / 2..rucksack.len()]
-            .chars()
-            .collect::<Vec<char>>();
-        let item_in_both = compartment1
-            .iter()
-            .filter(|&item| compartment2.contains(item))
-            .collect::<Vec<&char>>();
-
-        let offset = if item_in_both[0].is_ascii_uppercase() {
-            64 - 26 // 64 is the ascii code for A, and we want to start at 27
-        } else {
-            96
-        };
-        sum_of_priorities += item_in_both[0].clone() as u32 - offset;
+    input.lines().for_each(|pairs| {
+        let pairs_arr: Vec<&str> = pairs.split(",").collect();
+        let first_pair_start = pairs_arr[0].split("-").collect::<Vec<&str>>()[0];
+        let first_pair_end = pairs_arr[0].split("-").collect::<Vec<&str>>()[1];
+        let second_pair_start = pairs_arr[1].split("-").collect::<Vec<&str>>()[0];
+        let second_pair_end = pairs_arr[1].split("-").collect::<Vec<&str>>()[1];
+        
     });
-    println!("sum_of_priorities: {}", sum_of_priorities);
-    sum_of_priorities as i32
+    0
+
 }
